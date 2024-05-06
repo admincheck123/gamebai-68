95f610cfe5adab4529e86c8793e513e96877f3bf
admincheck123 <lek08670@gmail.com>
2024-05-06 22:12:10 +0700

    first commit

---
+++ config/database.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+	'url': 'mongodb://localhost:27017',
+	'options': {
+		//'user':   'kvcpro',
+		//'pass':   'Minhhoang2009@',
+		'dbName': 'RVIP', // red
+		'useNewUrlParser': true,
+		'useUnifiedTopology': true,
+		//'autoIndex':       false,
+	},
+};
